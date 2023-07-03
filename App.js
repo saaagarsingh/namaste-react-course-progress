a120d240601f380fcc99b57c0dc47fc62fbacc48
singhsagarsunil <singhsagarsunil@gmail.com>
2023-07-04 03:02:39 +0530

    Completed 2nd Session today

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
-const element = React.createElement("h1", {class:"looking", style:{color:"red", backgroundColor:"yellow"}}, "Namaste Duniya");
-const heading = React.createElement("h2", {id:"heading"}, "Hello World");
-const container = React.createElement("div", {class:"container"}, [element, heading]);
-console.log(element);
-ReactDOM.render(container, document.getElementById("root"));
+
+import React from "react";
+import ReactDOM from "react-dom";
+        
+const element = React.createElement("h1", {className:"looking", style:{color:"red", backgroundColor:"yellow"}, key:"1"}, "Namaste Duniya");
+const heading = React.createElement("h2", {id:"heading", key:"2"}, "Hello World");
+const container = React.createElement("div", {className:"container"}, [element, heading]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
